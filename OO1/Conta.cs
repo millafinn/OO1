9375c654782a61bbaeb3bf1de10713959a265938
camila.guimaraes <camila.guimaraes@SJ212-41>
2023-08-15 11:24:05 -0300

    Add project files.

---
+++ OO1/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OO1
+{
+    internal class Conta {
+        private int num;
+        private string nome;
+        private double saldo;
+
+
+        public void setNum(int num) {
+            this.num = num;
+        }
+
+        public int getNum() {
+            return num;
+        }
+
+        public void setNome(string nome) {
+            this.nome = nome;
+        }
+
+        public string getNome() {
+            return nome;
+        }
+
+        public void setSaldo(double saldo)  {
+            this.saldo = saldo;
+        }
+
+        public double getSaldo()  {
+            return saldo;
+        }
+
+    }
+}
---
+++ OO1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)  {
+            Conta conta1 = new Conta();
+            Conta conta2 = new Conta(); 
+
+            conta1.setNum(1);
+            conta1.setNome("LP"); //SET
+            conta1.setSaldo(1000);
+            Console.WriteLine(conta1.getNum());  //GET
+            Console.WriteLine(conta1.getNome());
+            Console.WriteLine(conta1.getSaldo());
+
+            conta2.setNum(2);
+            conta2.setNome("FM");
+            conta2.setSaldo(2000);
+            Console.WriteLine(conta2.getNum()); //GET
+            Console.WriteLine(conta2.getNome());
+            Console.WriteLine(conta2.getSaldo());
+        }
+    }
+}
